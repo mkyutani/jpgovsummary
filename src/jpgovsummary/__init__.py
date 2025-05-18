1c13a0e6f0d4b9f4e3c5c8663153c05e21ecd022
Miki Yutani <mkyutani@gmail.com>
2025-05-18 15:05:02 +0900

    refactor: remove unused route_tools.py (#2)

--- src/jpgovsummary/__init__.py
+++ src/jpgovsummary/__init__.py
@@ -1,7 +1,6 @@
 from .config import Config
 from .logger import logger
 from .model import Model
-from .route_tools import route_tools
 from .state import (
     CandidateReport,
     CandidateReportList,
